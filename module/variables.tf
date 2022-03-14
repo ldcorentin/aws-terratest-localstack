019842024d0f3eebc54f18313568f537e4f049a5
ldcorentin <ledevedeccorentin@gmail.com>
2022-03-14 20:15:34 +0100

    Remove experimental var

--- module/variables.tf
+++ module/variables.tf
@@ -1,9 +1,3 @@
-terraform {
-  # Optional attributes and the defaults function are 
-  # both experimental, so we must opt in to the experiment. 
-  experiments = [module_variable_optional_attrs]
-}
-
 variable "region" {
   default     = "eu-west-1"
   description = "The aws region."
